dd109e4c00b331b9df2587b2ff1217704f79e845
Tobi Delbruck <tobi@ini.uzh.ch>
2015-07-13 15:14:27 +0000

    Garrick Orchard contributions to allow reading in chunks of a large data file and extracting DVS events from a long DAVIS recording. Not tested yet by others.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@6935 b7f4320f-462c-0410-a916-d9f35bb82d52

--- matlab/davis/getDVSeventsDavis.m
+++ matlab/davis/getDVSeventsDavis.m
@@ -1,35 +1,89 @@
-function [x,y,pol,ts] = getDVSeventsDavis(file, x0, y0, x1, y1)  
-
-[addr ts]=loadaerdat(file);
+function [x,y,pol,ts] = getDVSeventsDavis(file, ROI, numEvents, startEvent)
+% function [x,y,pol,ts] = getDVSeventsDavis(file, ROI, numEvents, startEvent)
+% reads in events from the specified filename and returns only the DVS events
+%
+% file : The name of the file to read from
+%
+% ROI = [x0, y0, x1, y1] : Optional argument allowing a spatial region of interest to
+% specified
+%
+% numEvents: Optional argument which specifies the number of events to be read in (non-DVS events
+% will be filtered out, so the number of events returned will be less than
+% 'numEvents')
+%
+% startEvents: Specifies how many events to skip before reading begins.
+% This allows data to be read in chunks which is more memory efficient (because non-DVS event will be thrown out in each chunk)
+% For example:
+% chunkSize = 1e6; %number of events to read in per chunk
+% [x_chunk_1, y_chunk_1, pol_chunk_1, ts_chunk_1] = getDVSeventsDavis(file, [], chunkSize, 0*chunkSize);
+% [x_chunk_2, y_chunk_2, pol_chunk_2, ts_chunk_2] = getDVSeventsDavis(file, [], chunkSize, 1*chunkSize);
+% .
+% .
+% .
+% [x_chunk_N, y_chunk_N, pol_chunk_N, ts_chunk_N] = getDVSeventsDavis(file, [], chunkSize, (N-1)*chunkSize);
+fprintf('\ngetDVSeventsDavis function called \n')
 
 sizeX = 240;
 sizeY = 180;
 
-datamask = hex2dec ('3FF');
-readmask = hex2dec ('C00');
-readreset = hex2dec ('00');
-readsignal = hex2dec ('400');
+x0 = 0;
+y0 = 0;
+x1 = sizeX;
+y1 = sizeY;
+if exist('ROI', 'var')
+    if ~isempty(ROI)
+        if length(ROI) == 4
+            disp('Region of interest specified')
+            x0 = ROI(1);
+            y0 = ROI(2);
+            x1 = ROI(3);
+            y1 = ROI(4);
+        else
+            disp('Unknown ROI argument. Call function as: \n [x,y,pol,ts] = getDVSeventsDavis(file, [x0,y0,x1,y1], numEvents, startEvent) to specify ROI or\n getDVSeventsDavis(file, ~, numEvents, startEvent) to not specify ROI')
+            return
+        end
+    else
+        disp('No region of interest specified, reading in entire spatial area of sensor')
+    end
+else
+    disp('No region of interest specified, reading in entire spatial area of sensor')
+end
+
+
+if exist('numEvents', 'var')
+    disp(['Reading in at most ', num2str(numEvents)])
+    if exist('startEvent', 'var')
+        disp(['Starting reading from event ', num2str(startEvent)]);
+        [addr, ts]=loadaerdat(file, numEvents, startEvent);
+    else
+        disp('No starting points specified, starting reading from event 0 ');
+        [addr, ts]=loadaerdat(file, numEvents);
+    end
+else
+    disp('No maximum number of events specified. The (loadaerdat) function will automatically limit the number of events');
+    [addr, ts]=loadaerdat(file);
+end
+
+
+% datamask = hex2dec ('3FF');
+% readmask = hex2dec ('C00');
+% readreset = hex2dec ('00');
+% readsignal = hex2dec ('400');
 triggerevent = hex2dec ('400');
 polmask = hex2dec ('800');
-xmask = hex2dec ('3FF000'); 
-ymask = hex2dec ('7FC00000'); 
-typemask = hex2dec ('80000000'); 
+xmask = hex2dec ('003FF000');
+ymask = hex2dec ('7FC00000');
+typemask = hex2dec ('80000000');
 typedvs = hex2dec ('00');
-typeaps = hex2dec ('80000000'); 
-lasteventmask = hex2dec ('FFFFFC00');
-lastevent = hex2dec ('80000000');%starts with biggest address
-datashift = 0;
-xshift=12; 
+% typeaps = hex2dec ('80000000');
+% lasteventmask = hex2dec ('FFFFFC00');
+% lastevent = hex2dec ('80000000');%starts with biggest address
+% datashift = 0;
+xshift=12;
 yshift=22;
 polshift=11;
 
-
-if nargin < 5
-    x0 = 0;
-    y0 = 0;
-    x1 = sizeX;
-    y1 = sizeY;
-end
+numeventsread = length(addr);
 addr = abs(addr);
 ids = (addr&triggerevent) ~= triggerevent;
 addr = addr(ids);
@@ -39,24 +93,19 @@ addr = addr(ids);
 ts = ts(ids);
 
 
-xo=double(sizeX-1-bitshift(bitand(addr,xmask),-xshift));
-yo=double(bitshift(bitand(addr,ymask),-yshift));
+xo = sizeX-1-double(bitshift(bitand(addr,xmask), -xshift));
+yo = double(bitshift(bitand(addr,ymask),-yshift));
 polo=1-double(bitshift(bitand(addr,polmask),-polshift));
 
-% xo = xo(xo >= x0 & xo < x1);
-% yo = yo(yo >= y0 & yo < y1);
-% ids = intersect(find(xo), find(yo)); %dunnow if efficient
+
 ids = find(xo >= x0 & xo < x1 & yo >= y0 & yo < y1);
-% ids = find(ts);
 
 x = xo(ids);
 y = yo(ids);
 pol = polo(ids);
 ts = ts(ids);
 
-% x = xo;
-% y = yo;
-% pol = polo;
-% ts = ts;
 
+fprintf('Total number of events read = %i \n', numeventsread)
+fprintf('Total number of DVS events returned = %i \n', length(ids))
 end
--- matlab/loadaerdat.m
+++ matlab/loadaerdat.m
@@ -1,5 +1,5 @@
-function [allAddr,allTs]=loadaerdat(file,maxevents);
-%function [allAddr,allTs]=loadaerdat(file,n);
+function [allAddr,allTs]=loadaerdat(file, maxEvents, startEvent)
+%function [allAddr,allTs]=loadaerdat(file, maxEvents, startEvent);
 % loads events from a .dat file.
 %
 % allAddr are uint32 (or uint16 for legacy recordings) raw addresses.
@@ -11,40 +11,45 @@ function [allAddr,allTs]=loadaerdat(file,maxevents);
 % base workspace (in the case of no output argument).
 %
 % file is the input filename including path
-% maxevents is optional argument to specify maximum number of events loaded; maxevents default to 1e6.
-%
+% maxEvents is an optional argument to specify maximum number of events loaded; maxevents default to 1e6.
+% startEvent
 % Header lines starting with '#' are ignored and printed
 %
 % It is possible that the header parser can be fooled if the first
 % data byte is the comment character '#'; in this case the header must be
 % manually removed before parsing. Each header line starts with '#' and
 % ends with the hex characters 0x0D 0x0A (CRLF, windows line ending).
+fprintf('\nloadaerdat function called \n')
 
 defaultmaxevents=1e6;
 
-if nargin==2,
-       filename=file;    
-        path='';
-end
-if nargin==1,
-    if isstr(file),
-        path='';
-        filename=file;
-        maxevents=defaultmaxevents;
-    else
-        maxevents=file
-    end
-end
-
-if nargin==0,
-    maxevents=defaultmaxevents;
-    [filename,path,filterindex]=uigetfile({'*.*dat','*.aedat, *.dat'},'Select recorded retina data file');
+% check the input arguments
+if ~exist('file', 'var')
+    maxEvents = defaultmaxevents;
+    [filename,~,~]=uigetfile({'*.*dat','*.aedat, *.dat'},'Select recorded retina data file');
+    if filename==0, return; end
+elseif ischar(file)
+    filename = file;
+else
+    maxEvents = defaultmaxevents;
+    [filename,~,~]=uigetfile({'*.*dat','*.aedat, *.dat'},'Select recorded retina data file');
     if filename==0, return; end
 end
+   
+if ~exist('maxEvents', 'var')
+    maxEvents = defaultmaxevents;
+    disp(['Number of events not specified. Automatically limiting to reading ', num2str(maxEvents), ' events'])
+end
 
-fprintf('Reading at most %d events from file %s\n', maxevents,filename);
+if ~exist('startEvent', 'var')
+    startEvent = 0;
+    disp(['Starting point of events to read not specified. Beginning reading from event ', num2str(startEvent)])
+end
 
-f=fopen([path,filename],'r');
+
+fprintf('Reading at most %d events from file %s starting with event %d\n', maxEvents, filename, startEvent);
+
+f=fopen(filename,'r');
 % skip header lines
 bof=ftell(f);
 line=native2unicode(fgets(f));
@@ -55,7 +60,9 @@ while line(1)=='#',
     if strncmp(line,tok, length(tok))==1,
         version=sscanf(line(length(tok)+1:end),'%f');
     end
-    fprintf('%s',line); % print line using \n for newline, discarding CRLF written by java under windows
+    %(Garrick Orchard) commenting out printing of header, which makes other
+    %printouts from this function difficult to find
+%     fprintf('%s',line); % print line using \n for newline, discarding CRLF written by java under windows
     bof=ftell(f); % save end of comment header location
     line=native2unicode(fgets(f)); % gets the line including line ending chars
 end
@@ -66,40 +73,44 @@ switch version,
         version=1;
     case 1
         fprintf('Addresses are 16 bit with version 1 AER-DAT file format\n');
+        version=1;
     case 2
         fprintf('Addresses are 32 bit with version 2 AER-DAT file format\n');
+        version=2;
     otherwise
         fprintf('Unknown AER-DAT file format version %g',version);
 end
-
+% version
 numBytesPerEvent=6;
 switch(version)
     case 1
         numBytesPerEvent=6;
+        addr_Skip = 4; %how many bytes between each address
+        addr_precision = 'uint16';
+        T_Skip = 2; %how many bytes between each timestamp
+        T_precision = 'uint32';
     case 2
         numBytesPerEvent=8;
+        addr_Skip = 4; %how many bytes between each address
+        addr_precision = 'uint32';
+        T_Skip = 4; %how many bytes between each timestamp
+        T_precision = 'uint32';
+%         disp('correctly assigned bits and skips')
 end
 
         
 fseek(f,0,'eof');
-numEvents=floor((ftell(f)-bof)/numBytesPerEvent); % 6 or 8 bytes/event
-if numEvents>maxevents, 
-    fprintf('clipping to %d events although there are %d events in file\n',maxevents,numEvents);
-    numEvents=maxevents;
+numEvents=floor((ftell(f)-bof-numBytesPerEvent*startEvent)/numBytesPerEvent); % 6 or 8 bytes/event
+if numEvents>maxEvents, 
+    fprintf('clipping to %d events although there are %d events in file\n',maxEvents,numEvents);
+    numEvents=maxEvents;
 end
 
 % read data
-fseek(f,bof,'bof'); % start just after header
-switch version,
-    case 1
-        allAddr=uint16(fread(f,numEvents,'uint16',4,'b')); % addr are each 2 bytes (uint16) separated by 4 byte timestamps
-        fseek(f,bof+2,'bof'); % timestamps start 2 after bof
-        allTs=uint32(fread(f,numEvents,'uint32',2,'b')); % ts are 4 bytes (uint32) skipping 2 bytes after each
-    case 2
-        allAddr=uint32(fread(f,numEvents,'uint32',4,'b')); % addr are each 4 bytes (uint32) separated by 4 byte timestamps
-        fseek(f,bof+4,'bof'); % timestamps start 4 after bof
-        allTs=uint32(fread(f,numEvents,'uint32',4,'b')); % ts are 4 bytes (uint32) skipping 4 bytes after each
-end
+fseek(f,bof+numBytesPerEvent*startEvent,'bof'); % start 'startEvents' after header 
+allAddr=uint32(fread(f,numEvents,addr_precision,addr_Skip,'b')); % addr are each 2 bytes (uint16) separated by 4 byte timestamps
+fseek(f,bof+numBytesPerEvent*startEvent+T_Skip,'bof'); % start 'startEvents' plus the Timestamp offset after header 
+allTs=uint32(fread(f,numEvents,T_precision,T_Skip,'b')); % ts are 4 bytes (uint32) skipping 2 bytes after each
 
 fclose(f);
 
