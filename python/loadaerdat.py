77cbd92c7389dc138fa612dfb1b5a3403b67fd73
Federico Corradi <federico.corradi@inilabs.com>
2015-08-27 08:56:16 +0000

    python scripts: added support for loading DAVIS240x aerdat files, now there is an option in function defition which refears to camera type DVS128 or DAVIS240
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@7103 b7f4320f-462c-0410-a916-d9f35bb82d52

--- python/loadaerdat.py
+++ python/loadaerdat.py
@@ -1,8 +1,9 @@
 import struct
 import os
 
-def loadaerdat(datafile='/tmp/aerout.dat',length=0,version="aedat", debug=1):
+def loadaerdat(datafile='/tmp/aerout.dat',length=0,version="aedat", debug=1, camera='DVS128'):
     """
+    
     load AER data file and parse these properties of AE events:
     - timestamps (in us), 
     - x,y-position [0..127]
@@ -12,18 +13,31 @@ def loadaerdat(datafile='/tmp/aerout.dat',length=0,version="aedat", debug=1):
     @param length - how many bytes(B) should be read; default 0=whole file
     @param version - which file format version is used: "aedat" = v2, "dat" = v1 (old)
     @param debug - 0 = silent, 1 (default) = print summary, >=2 = print all debug
+    @param camera='DVS128' or 'DAVIS240'
     @return (ts, xpos, ypos, pol) 4-tuple of lists containing data of all events;
     """
-    # constants
-    xmask = 0x00fe
-    xshift = 1
-    ymask = 0x7f00
-    yshift = 8
+    if(camera == 'DVS128'):
+        # constants
+        xmask = 0x00fe
+        xshift = 1
+        ymask = 0x7f00
+        yshift = 8
+
+    if(camera == 'DAVIS240'):   
+        # constants
+        xmask = 0x03ff
+        xshift = 12
+        ymask = 0x01ff
+        yshift = 22
+        polshift = 11
+        eventypeshift = 31
+    
     typemask = 1
     typeshift = 0
     aeLen = 8 # 1 AE event takes 8 bytes
     readMode = '>II' # struct.unpack(), 2x ulong, 4B+4B
-    td = 0.000001 # timestep is 1us
+    td = 0.000001 # timestep is 1us    
+    
     if version == "dat":
       print "using the old .dat format"
       aeLen = 6
@@ -58,21 +72,46 @@ def loadaerdat(datafile='/tmp/aerout.dat',length=0,version="aedat", debug=1):
     aerdatafh.seek(p)
     s = aerdatafh.read(aeLen)
     p+=aeLen
-    while p < length:
-        addr, ts = struct.unpack(readMode, s)
-        if debug >=3: 
-          print addr,ts
-        x = (addr&xmask)>>(xshift)
-        y = (addr&ymask)>>(yshift)
-        type = (addr&typemask)>>typeshift
-        timestamps.append(ts)
-        xaddr.append(x)
-        yaddr.append(y)
-        pol.append(type)
+    
+    if(camera == 'DVS128'):
+        while p < length:
+            addr, ts = struct.unpack(readMode, s)
+            if debug >=3: 
+              print addr,ts
+            x = (addr&xmask)>>(xshift)
+            y = (addr&ymask)>>(yshift)
+            type = (addr&typemask)>>typeshift
+            timestamps.append(ts)
+            xaddr.append(x)
+            yaddr.append(y)
+            pol.append(type)
 
-        aerdatafh.seek(p)
-        s = aerdatafh.read(aeLen)
-        p+=aeLen
+            aerdatafh.seek(p)
+            s = aerdatafh.read(aeLen)
+            p+=aeLen
+            
+    if(camera == 'DAVIS240'):
+        while p < length:
+            addr, ts = struct.unpack(readMode, s)
+            eventtype = (addr >> eventypeshift) 
+            if(eventtype == 0): #this is a DVS event
+                x_addr = (addr >> xshift) & xmask
+                y_addr = (addr >> yshift) & ymask
+                a_pol = (addr >> polshift) & 0x1
+
+                if debug >=3: 
+                    print("ts->", ts) #ok
+                    print("x-> ", x_addr)
+                    print("y-> ", y_addr)
+                    print("pol->", a_pol)
+                timestamps.append(ts)
+                xaddr.append(x_addr)
+                yaddr.append(y_addr)
+                pol.append(a_pol)
+                  
+            aerdatafh.seek(p)
+            s = aerdatafh.read(aeLen)
+            p+=aeLen        
 
     if debug > 0:
       try:
