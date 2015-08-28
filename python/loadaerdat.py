5e3d7694ca7b07cdc3de33fb28d14f7039b8b521
Marek Otahal <markotahal@gmail.com>
2015-08-28 19:02:38 +0000

    python/loadaerdat.py: cleanup code
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@7111 b7f4320f-462c-0410-a916-d9f35bb82d52

--- python/loadaerdat.py
+++ python/loadaerdat.py
@@ -1,9 +1,14 @@
 import struct
 import os
 
-def loadaerdat(datafile='/tmp/aerout.dat',length=0,version="aedat", debug=1, camera='DVS128'):
-    """
-    
+V2 = "aedat" # current 32bit file format
+V1 = "dat" # old format
+
+EVT_DVS = 0 # DVS event type
+EVT_APS = 1 # APS event
+
+def loadaerdat(datafile='/tmp/aerout.dat', length=0, version=V2, debug=1, camera='DVS128'):
+    """    
     load AER data file and parse these properties of AE events:
     - timestamps (in us), 
     - x,y-position [0..127]
@@ -16,29 +21,30 @@ def loadaerdat(datafile='/tmp/aerout.dat',length=0,version="aedat", debug=1, cam
     @param camera='DVS128' or 'DAVIS240'
     @return (ts, xpos, ypos, pol) 4-tuple of lists containing data of all events;
     """
+    # constants
+    aeLen = 8 # 1 AE event takes 8 bytes
+    readMode = '>II' # struct.unpack(), 2x ulong, 4B+4B
+    td = 0.000001 # timestep is 1us   
     if(camera == 'DVS128'):
-        # constants
         xmask = 0x00fe
         xshift = 1
         ymask = 0x7f00
         yshift = 8
-
-    if(camera == 'DAVIS240'):   
-        # constants
-        xmask = 0x03ff
+        pmask = 0x1
+        pshift = 0
+    elif(camera == 'DAVIS240'):  # values take from scripts/matlab/getDVS*.m
+        xmask = 0x003ff000
         xshift = 12
-        ymask = 0x01ff
+        ymask = 0x7fc00000
         yshift = 22
-        polshift = 11
-        eventypeshift = 31
+        pmask = 0x800
+        pshift = 11
+        eventtypeshift = 31
+    else:
+        raise ValueError("Unsupported camera: %s" % (camera))
     
-    typemask = 1
-    typeshift = 0
-    aeLen = 8 # 1 AE event takes 8 bytes
-    readMode = '>II' # struct.unpack(), 2x ulong, 4B+4B
-    td = 0.000001 # timestep is 1us    
-    
-    if version == "dat":
+   
+    if version == V1:
       print "using the old .dat format"
       aeLen = 6
       readMode = '>HI' #ushot, ulong = 2B+4B
@@ -73,52 +79,43 @@ def loadaerdat(datafile='/tmp/aerout.dat',length=0,version="aedat", debug=1, cam
     s = aerdatafh.read(aeLen)
     p+=aeLen
     
-    if(camera == 'DVS128'):
-        while p < length:
-            addr, ts = struct.unpack(readMode, s)
+    print xmask, xshift, ymask, yshift, pmask, pshift    
+    while p < length:
+        addr, ts = struct.unpack(readMode, s)
+        # parse event type
+        if(camera == 'DAVIS240'):
+            eventtype = (addr >> eventtypeshift)
+        else: # DVS128
+            eventtype = EVT_DVS
+        
+        # parse event's data
+        if(eventtype == EVT_DVS): #this is a DVS event
+            x_addr = (addr & xmask) >> xshift
+            y_addr = (addr & ymask) >> yshift
+            a_pol =  (addr & pmask) >> pshift
+
+
             if debug >=3: 
-              print addr,ts
-            x = (addr&xmask)>>(xshift)
-            y = (addr&ymask)>>(yshift)
-            type = (addr&typemask)>>typeshift
+                print("ts->", ts) #ok
+                print("x-> ", x_addr)
+                print("y-> ", y_addr)
+                print("pol->", a_pol)
+
             timestamps.append(ts)
-            xaddr.append(x)
-            yaddr.append(y)
-            pol.append(type)
-
-            aerdatafh.seek(p)
-            s = aerdatafh.read(aeLen)
-            p+=aeLen
-            
-    if(camera == 'DAVIS240'):
-        while p < length:
-            addr, ts = struct.unpack(readMode, s)
-            eventtype = (addr >> eventypeshift) 
-            if(eventtype == 0): #this is a DVS event
-                x_addr = (addr >> xshift) & xmask
-                y_addr = (addr >> yshift) & ymask
-                a_pol = (addr >> polshift) & 0x1
-
-                if debug >=3: 
-                    print("ts->", ts) #ok
-                    print("x-> ", x_addr)
-                    print("y-> ", y_addr)
-                    print("pol->", a_pol)
-                timestamps.append(ts)
-                xaddr.append(x_addr)
-                yaddr.append(y_addr)
-                pol.append(a_pol)
+            xaddr.append(x_addr)
+            yaddr.append(y_addr)
+            pol.append(a_pol)
                   
-            aerdatafh.seek(p)
-            s = aerdatafh.read(aeLen)
-            p+=aeLen        
+        aerdatafh.seek(p)
+        s = aerdatafh.read(aeLen)
+        p+=aeLen        
 
     if debug > 0:
       try:
         print "read %i (~ %.2fM) AE events, duration= %.2fs" % (len(timestamps), len(timestamps)/float(10**6), (timestamps[-1]-timestamps[0])*td)
         n = 5
         print "showing first %i:" % (n) 
-        print "timestamps: %s \nX-addr: %s\nY-addr: %s\npolarity: %s" % (timestamps[1:n], xaddr[1:n], yaddr[1:n], pol[1:n])
+        print "timestamps: %s \nX-addr: %s\nY-addr: %s\npolarity: %s" % (timestamps[0:n], xaddr[0:n], yaddr[0:n], pol[0:n])
       except:
         print "failed to print statistics"
 
