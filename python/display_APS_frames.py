242fb81274d968aa0285a44eae9e3c1793a15a51
Federico Corradi <federico.corradi@inilabs.com>
2015-09-04 14:09:30 +0000

    python scripts: added scripts for reading packets that are streamed via jAER-UDP, also added function that displays frames
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@7147 b7f4320f-462c-0410-a916-d9f35bb82d52

---
+++ python/display_APS_frames.py
@@ -0,0 +1,27 @@
+###############################
+## plot frame images obtained
+## from net_raw_UDP_jAER
+## federico.corradi@inilabs.com
+###############################
+
+import matplotlib as mplt
+import pylab as plt
+from time import sleep
+
+def display_APS_frames(frames, time_interval=1):
+    '''
+    display DAVIS240 frames with fixed time interval
+    inputs:
+        frames : obtained from net_raw_UDP_jAER.py
+        time_interval : optional parameter, time interval in between frames
+    '''
+    plt.ion()
+    my_obj = plt.imshow(plt.rot90(frames[1][3,:,:]))
+    plt.hold('on')
+    for i in range(len(frames)):
+        frames[i][3, :, :] = frames[i][2,:,:] - frames[i][1,:,:]
+        frames[i][6, :, :] = frames[i][5,:,:] - frames[i][4,:,:]
+        frames[i][3,:,:][frames[i][3,:,:] < 0] = 0
+        my_obj.set_data(plt.rot90(frames[i][3,:,:]))
+        plt.draw()
+        sleep(1)
