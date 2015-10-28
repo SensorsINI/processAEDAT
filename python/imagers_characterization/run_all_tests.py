7b6016918b32fd73764409e3beb7efaff4f05a57
Federico Corradi <federico.corradi@inilabs.com>
2015-10-28 18:20:35 +0000

    python scipts: measurements data folder and sine wave
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@7522 b7f4320f-462c-0410-a916-d9f35bb82d52

--- python/imagers_characterization/run_all_tests.py
+++ python/imagers_characterization/run_all_tests.py
@@ -6,16 +6,29 @@ from __future__ import division
 import numpy as np
 import matplotlib
 from pylab import *
+import time, os
 
 # import caer communication and control gpib/usb instrumentations
 import caer_communication
 import gpio_usb
+
+# TEST SELECTION
+do_ptc = True
+do_fpn = True
+current_date = time.strftime("%d_%m_%y-%H_%M_%S")
+datadir = 'measurements'
+
+
 
 # 0 - INIT control tools
 # init control class and open communication
 control = caer_communication.caer_communication(host='localhost')
 gpio_cnt = gpio_usb.gpio_usb()
 print gpio_cnt.query(gpio_cnt.fun_gen,"*IDN?")
+try:
+    os.stat(datadir)
+except:
+    os.mkdir(datadir) 
 
 ################################################################################
 ## CONDITION 1 - Homegeneous light source
@@ -25,18 +38,22 @@ print gpio_cnt.query(gpio_cnt.fun_gen,"*IDN?")
 
 # 1 - Photon Transfer Curve - data
 # setup is in conditions -> Homegeneous light source (integrating sphere, need to measure the luminosity)
-control.open_communication_command()
-control.load_biases()    
-control.get_data_ptc(folder='ptc', recording_time=3, exposures=np.linspace(100,5000,3))
-control.close_communication_command()    
+if do_ptc:
+    control.open_communication_command()
+    control.load_biases()    
+    folder = datadir + '/ptc_' +  current_date
+    control.get_data_ptc( folder = folder, recording_time=3, exposures=np.linspace(100,5000,6))
+    control.close_communication_command()    
 
 # 2 - Fixed Pattern Noise - data
 # setup is in conditions -> Homegeneous light source (integrating sphere, need to measure the luminosity)
 # + we slowly generate a sine wave 
-control.open_communication_command()
-control.load_biases()    
-control.get_data_fpn(folder='fpn', recording_time=20, exposure=1000 )
-gpio_cnt.set_inst(gpio_cnt.fun_gen,"APPL:SIN 0.1, 0.1, 0") #0.1 Vpp sine wave at 1 Hz with a 0 volt offset
-control.close_communication_command()    
+if do_fpn:
+    control.open_communication_command()
+    control.load_biases()    
+    folder = datadir + '/fpn_' +  current_date
+    control.get_data_fpn(folder = folder, recording_time=20, exposure=1000 )
+    gpio_cnt.set_inst(gpio_cnt.fun_gen,"APPL:SIN 2, 0.1, 0") #0.1 Vpp sine wave at 1 Hz with a 0 volt offset
+    control.close_communication_command()    
 
 
