committing changes: fix bug in True/true spelling in caer_communication and minor fixes and gain calculation in aedat3_process. I adapted run_alltests to pixelparade
git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@7759 b7f4320f-462c-0410-a916-d9f35bb82d52

python/cAER_utils/imagers_characterization/aedat3_process.py
@@ -1,6 +1,7 @@
 # ############################################################
 # python class that deals with cAER aedat3 file format
 # author  Federico Corradi - federico.corradi@inilabs.com
+# author  Diederik Paul Moeys - diederikmoeys@live.com
 # ############################################################
 from __future__ import division
 import os
@@ -375,6 +376,8 @@ class aedat3_process:
             this_mean_values = u_y_tot.reshape([un,una])[:,this_area]
             this_mean_values_lin = this_mean_values[0:max_ind_var]
             slope, inter = np.polyfit(this_mean_values_lin.reshape(len(this_mean_values_lin)),sigma_fit.reshape(len(sigma_fit))[0:max_ind_var],1)
+            print("sl"+str(slope))
+            print("Gain: "+str(format(((ADC_range*slope)/ADC_values)*1000000, '.1f'))+"uV/e for area: "+ str(frame_x_divisions[this_area]) )
             fit_fn = np.poly1d([slope, inter]) 
             ax.plot( u_y_tot.reshape([un,una])[:,this_area] , sigma_tot.reshape([un,una])[:,this_area] , 'o--', color=colors[this_area], label='pixel area' + str(frame_x_divisions[this_area]) )
             ax.plot(this_mean_values_lin.reshape(len(this_mean_values_lin)), fit_fn(this_mean_values_lin.reshape(len(this_mean_values_lin))), '-*', markersize=4, color=colors[this_area])
@@ -384,7 +387,6 @@ class aedat3_process:
         plt.ylabel('Var[DN]  ')     
         plt.savefig(figure_dir+"ptc_linear_fit.pdf",  format='pdf') 
         plt.savefig(figure_dir+"ptc_linear_fit.png",  format='png')
-	print("Slope: "+str(format(slope, '.5f'))+" Intercept: "+str(format(inter, '.3f'))  )
         #plt.close()
         
         
@@ -1081,10 +1083,14 @@ if __name__ == "__main__":
     do_fpn = False
     do_latency_pixel = False
     do_contrast_sensitivity = False
-    directory_meas = 'measurements/DAVIS240C_ptc_16_12_15-15_56_33/'
-    camera_dim = [240,180]
-    frame_x_divisions = [[50,100]]#[[120,121], [121,122]]#[[0,20], [20,190], [190,210], [210,220], [220,230], [230,240]]
-    frame_y_divisions = [[50,100]]#[[121,122]]#[[0,180]]
+    directory_meas = 'measurements/DAVIS208_ptc_22_12_15-15_50_29/'
+    camera_dim = [208, 192] #Pixelparade 208Mono 
+	#[240,180] #DAVSI240C
+    ADC_range = 1.520 # For PixelParade 208Mono measure the voltage between E1 and F2
+    ADC_values = 1024
+    frame_x_divisions = [[207-5,207-0], [207-12,207-8], [207-18,207-15], [207-207,207-19]] # Pixelparade 208Mono since it is flipped sideways
+# DAVIS240 C [[120,121], [121,122]]#[[0,20], [20,190], [190,210], [210,220], [220,230], [230,240]]
+    frame_y_divisions = [[0,191]]#[[121,122]]#[[0,180]]
     ################### 
     # END PARAMETERS
     ###################

python/cAER_utils/imagers_characterization/caer_communication.py
@@ -427,7 +427,7 @@ class caer_communication:
 
         return
 
-    def get_data_ptc(self, folder = 'ptc', recording_time = 5,  exposures = np.linspace(1,1000,5), global_shutter=True, sensor_type = "DAVISFX2", useinternaladc = False):
+    def get_data_ptc(self, folder = 'ptc', recording_time = 5,  exposures = np.linspace(1,1000,5), global_shutter=True, sensor_type = "DAVISFX2", useinternaladc = True):
         '''
             this function get the data for the Photon Transfer Curve measure - 
             it requires an APS camera
@@ -443,6 +443,7 @@ class caer_communication:
             os.mkdir(folder) 
         #loop over exposures and save data
         for this_exp in range(len(exposures)):
+            self.send_command('put /1/1-'+str(sensor_type)+'/aps/ UseInternalADC bool '+str.lower(str(useinternaladc)))
             if(np.round(exposures[this_exp]) == 0):
                 print "exposure == 0 is not valid, skipping this step..."
             else:

python/cAER_utils/imagers_characterization/run_all_tests.py
@@ -1,6 +1,7 @@
 # ############################################################
 # python class that runs experiments and save data
 # author  Federico Corradi - federico.corradi@inilabs.com
+# author  Diederik Paul Moeys - diederikmoeys@live.com
 # ############################################################
 from __future__ import division
 import numpy as np
@@ -30,14 +31,14 @@ current_date = time.strftime("%d_%m_%y-%H_%M_%S")
 datadir = 'measurements'
 useinternaladc = True
 global_shutter = True
-exposures = np.linspace(1,100,25)
+exposures = np.linspace(1,1000,25)
 
 ###############################################################################
 # CAMERA SELECTION and SETUP PARAMETERS
 ###############################################################################
-sensor = "DAVIS240C"
+sensor = "DAVIS208"
 sensor_type = "DAVISFX3"
-bias_file = "cameras/davis240c_standards.xml"
+bias_file = "cameras/davis208Mono.xml"
 host_ip = '172.19.11.139'
 
 ##############################################################################
@@ -59,13 +60,19 @@ if plot_setup_characterization:
     plot(volt, volt*slope+inter, 'k-', label='fit linear')
     legend(loc='best')
 
-#####################################################################
+##############################################################################
 # 0 - INIT control tools
 # init control class and open communication
-#####################################################################
+##############################################################################
 control = caer_communication.caer_communication(host=host_ip)
 gpio_cnt = gpio_usb.gpio_usb()
 print gpio_cnt.query(gpio_cnt.fun_gen,"*IDN?")
+
+gpio_cnt.set_inst(gpio_cnt.k230,"I0M1D0F1X") 
+gpio_cnt.set_inst(gpio_cnt.k230,"I2X") # set current limit to max
+gpio_cnt.set_inst(gpio_cnt.k230,"V"+str(0)) #voltage output
+gpio_cnt.set_inst(gpio_cnt.k230,"F1X") #operate
+
 try:
     os.stat(datadir)
 except:
@@ -81,11 +88,11 @@ def copyFile(src, dest):
     except IOError as e:
         print('Error: %s' % e.strerror)
 
-################################################################################
+##############################################################################
 ## CONDITION 1 - Homegeneous light source
 ## Homegeneous light source (integrating sphere, need to measure the luminosity)
 ## also use the hp33120 to generate sine wave at .1 Hz for FPN measurements
-#################################################################################
+##############################################################################
 
 # 1 - Photon Transfer Curve - data
 # setup is in conditions -> Homegeneous light source (integrating sphere, need to measure the luminosity)
@@ -94,13 +101,21 @@ if do_ptc:
     print "we are doing ptc measurements, please put homogeneous light source (integrating sphere)."
     raw_input("Press Enter to continue...")
     control.open_communication_command()
+    # Zero the Function Generator
+    gpio_cnt.set_inst(gpio_cnt.fun_gen,"APPL:DC DEF, DEF, 0")
+    # Set the K230 to the chosen luminosity
+    gpio_cnt.set_inst(gpio_cnt.k230,"I0M1D0F1X") 
+    gpio_cnt.set_inst(gpio_cnt.k230,"I2X") # set current limit to max
+    v_base_level = (base_level - inter) / slope
+    gpio_cnt.set_inst(gpio_cnt.k230,"V"+str(v_base_level)) #voltage output
+    gpio_cnt.set_inst(gpio_cnt.k230,"F1X") #operate
     folder = datadir + '/'+ sensor + '_ptc_' +  current_date
     setting_dir = folder + str("/settings/")
     if(not os.path.exists(setting_dir)):
         os.makedirs(setting_dir)
     control.load_biases(xml_file=bias_file)
     copyFile(bias_file, setting_dir+str("biases_ptc_all_exposures.xml") )
-    control.get_data_ptc( folder = folder, recording_time=3, exposures=exposures, global_shutter=global_shutter, sensor_type = sensor_type, useinternaladc = False )
+    control.get_data_ptc( folder = folder, recording_time=3, exposures=exposures, global_shutter=global_shutter, sensor_type = sensor_type, useinternaladc = useinternaladc )
     control.close_communication_command()    
     print "Data saved in " +  folder
 
@@ -178,5 +193,9 @@ if do_latency_pixel:
     control.close_communication_command()    
     print "Data saved in " +  folder
 
+gpio_cnt.set_inst(gpio_cnt.k230,"I0M1D0F1X") 
+gpio_cnt.set_inst(gpio_cnt.k230,"I2X") # set current limit to max
+gpio_cnt.set_inst(gpio_cnt.k230,"V"+str(0)) #voltage output
+gpio_cnt.set_inst(gpio_cnt.k230,"F1X") #operate
 
 