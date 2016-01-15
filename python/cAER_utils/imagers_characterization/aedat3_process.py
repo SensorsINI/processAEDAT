3afd1349dc3a4dd0cba3abdaf69510902f84968e
Federico Corradi <federico.corradi@inilabs.com>
2016-01-15 11:20:44 +0000

    python scripts: camera oscillations data analysis fix
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@7845 b7f4320f-462c-0410-a916-d9f35bb82d52

--- python/cAER_utils/imagers_characterization/aedat3_process.py
+++ python/cAER_utils/imagers_characterization/aedat3_process.py
@@ -1419,10 +1419,11 @@ if __name__ == "__main__":
     do_ptc = False
     do_fpn = False
     do_latency_pixel = False
-    do_contrast_sensitivity = True
+    do_contrast_sensitivity = False
     do_oscillations = True      #for NW
-    directory_meas = 'measurements/Measurements_final/208Mono/davis208 plenty of points/DARK/DAVIS208Mono_ADCint_ptc_dark_13_01_16-13_49_24/'
-    camera_dim =  [208,192] #Pixelparade 208Mono 
+    directory_meas = 'measurements/Measurements_final/DAVIS240C/DAVIS240C_oscillations_15_01_16-12_13_37/'
+    camera_dim = [240,180]
+    #[208,192] #Pixelparade 208Mono 
     #[240,180] #DAVSI240C
     # http://www.ti.com/lit/ds/symlink/ths1030.pdf (External ADC datasheet)
     # 0.596 internal adcs 346B
@@ -1432,13 +1433,14 @@ if __name__ == "__main__":
     # 0.648 external adcs reference is the same for all chips
     ADC_range = 1.29#0.648#240C 1.501
     ADC_values = 1024
-    frame_x_divisions = [[207-3,207-0], [207-5,207-4], [207-9,207-8], [207-11,207-10], [207-13,207-12], [207-19,207-16], [207-207,207-20]] 
+    frame_x_divisions = [[0,20], [20,190], [190,210], [210,220], [220,230], [230,240]]
     #   Pixelparade 208 Mono since it is flipped sideways (don't include last number in python)
     #   208Mono (Pixelparade)   [[207-3,207-0], [207-5,207-4], [207-9,207-8], [207-11,207-10], [207-13,207-12], [207-19,207-16], [207-207,207-20]] 
     #   240C                    [[0,20], [20,190], [190,210], [210,220], [220,230], [230,240]]
-    frame_y_divisions = [[0,191]]
-    #   [[121,122]] 640Color
-    #   [[0,180]]
+    frame_y_divisions = [[0,180]]
+	#   208Mono 	[[0,191]]
+    #   640Color 	[[121,122]] 
+    #   240C		[[0,180]]
     # 
     # ###############################
     # contrast sensitivity parameter
@@ -1454,7 +1456,7 @@ if __name__ == "__main__":
         if(not os.path.exists(figure_dir)):
             os.makedirs(figure_dir)
         aedat = aedat3_process()
-        all_latencies_mean_up, all_latencies_mean_dn, all_latencies_std_up, all_latencies_std_dn = aedat.oscillations_latency_analysis(latency_pixel_dir, figure_dir, camera_dim = [240,180], size_led = 2, file_type="cAER", confidence_level=0.95) #pixel size of the led
+        all_latencies_mean_up, all_latencies_mean_dn, all_latencies_std_up, all_latencies_std_dn = aedat.oscillations_latency_analysis(oscil_dir, figure_dir, camera_dim = [240,180], size_led = 2, file_type="cAER", confidence_level=0.95) #pixel size of the led
         print("latency up :", all_latencies_mean_up, " std ", all_latencies_std_up)
         print("latency dn :", all_latencies_mean_dn, " std ", all_latencies_std_dn)
 
