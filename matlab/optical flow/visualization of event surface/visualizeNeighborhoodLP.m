3e9291ef9b56c85aa85a5b3784c9e110e0524753
Bodo Rückauer <rbodo@ini.uzh.ch>
2016-01-15 07:10:41 +0000

    Generalized this script to arbitrary neighborhood sizes and approprately label plots.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@7843 b7f4320f-462c-0410-a916-d9f35bb82d52

--- matlab/optical flow/visualization of event surface/visualizeNeighborhoodLP.m
+++ matlab/optical flow/visualization of event surface/visualizeNeighborhoodLP.m
@@ -7,35 +7,64 @@
 clearvars;
 
 % Copy this line from the jAER output log and insert it here:
-T = [[122.0, 125.0, 0.007794999983161688, 1.0]; [123.0, 124.0, 0.017607999965548515, 1.0]; [124.0, 125.0, 0.0024969999212771654, 1.0]; [124.0, 126.0, -0.009542999789118767, 1.0]; ]; pe = [-0.00 -0.01 -0.47 0.88]; v = [0.00 -81.46]; vIMU = [0.19 -0.10];
+T = [[0 75 0.00];[0 76 0.00];[0 77 0.00];[0 78 0.00];[0 79 0.00];[0 80 0.00];[0 81 0.00];[1 75 0.00];[1 76 0.00];[1 77 0.00];[1 78 0.00];[1 79 0.00];[1 80 0.00];[1 81 0.00];[2 75 0.00];[2 76 0.00];[2 77 0.00];[2 78 0.00];[2 79 0.00];[2 80 0.00];[2 81 0.00];[3 75 0.00];[3 76 0.00];[3 77 1.53];[3 78 1.57];[3 79 0.00];[3 80 0.00];[3 81 0.00];[4 75 1.49];[4 76 0.00];[4 77 0.79];[4 78 0.00];[4 79 0.00];[4 80 1.56];[4 81 0.70];[5 75 1.47];[5 76 0.00];[5 77 1.49];[5 78 0.00];[5 79 0.00];[5 80 0.00];[5 81 0.70];[6 75 0.00];[6 76 1.53];[6 77 1.51];[6 78 0.00];[6 79 1.54];[6 80 0.69];[6 81 0.00];]; pe = [-3193.66 2295.06 249253.24]; v = [-313.12 435.72]; vIMU = [-8.64 62.71];
 
-% Restructure data to fit MATLAB conventions
-X = T(:,1);
-Y = T(:,2);
+searchDistance = 3; % Spatial window: pixel in each direction
+maxDtThreshold = 0.1; % Temporal window in seconds
+neighbSize = (2*searchDistance+1)*(2*searchDistance+1);
+X = T(:,1)+1; % Add one to convert from java to matlab indices
+Y = T(:,2)+1;
 Z = T(:,3);
-[x,y] = meshgrid(linspace(min(X),min(X)+2,3),linspace(min(Y),min(Y)+2,3));
-z = -(pe(1)*x + pe(2)*y + pe(4))/pe(3);
+%[x,y] = meshgrid(min(X):min(X)+2*searchDistance,min(Y):min(Y)+2*searchDistance);
+[x,y] = meshgrid(-searchDistance:searchDistance,-searchDistance:searchDistance);
+y = -1*y;
+t = zeros(size(x));
+
+for i = 1:numel(Z)
+    t(Y(i),X(i)) = Z(i);
+end
+
+% For the Savitzky-Golay variant, the plane estimate consists of three
+% instead of four parameters:
+if numel(pe) < 4
+    z = (pe(1)*x + pe(2)*y + pe(3))*1e-6;
+else
+    z = -(pe(1)*x + pe(2)*y + pe(4))/pe(3);
+end
 
 % Plot surface of active events 
 figure
-hold on
-surf(x,y,z,gradient(z))
-
-% Try a single fit (the base method in jAER uses iterative improvement)
-sf = fit([X,Y],Z,'poly11');
-plot(sf,[X,Y],Z)
-
-ax = gca;
-ax.XTick = min(X):1:min(X)+3;
-ax.YTick = min(Y):1:min(Y)+3;
+subplot(2,2,1)
+surf(t)
+axis([min(X) min(X)+2*searchDistance+1 min(Y) min(Y)+2*searchDistance+1 0 inf])
 xlabel('x')
 ylabel('y')
 zlabel('time [s]')
-title('surface of most recent events')
-hold off
+title('Surface of most recent events')
+
+subplot(2,2,2)
+surf(x,y,z,gradient(z))
+xlabel('x')
+ylabel('y')
+zlabel('time [s]')
+title('Plane estimate')
+
+% Try a single fit (the base method in jAER uses iterative improvement)
+ind = find(Z > max(Z) - maxDtThreshold);
+sf = fit([X(ind),Y(ind)],Z(ind),'poly11');
+subplot(2,2,3)
+plot(sf,[X(ind),Y(ind)],Z(ind))
+xlabel('x')
+ylabel('y')
+zlabel('time [s]')
+title('Single Fit')
 
 % Plot the estimated flow from jAER, the flow computed with the single
 % plane fit above, and the ground truth.
-figure
-vM = [1/sf.p10,1/sf.p01];
-compass([v(1),vM(1),vIMU(1)],[v(2),vM(2),vIMU(2)])
+vSF = [1/sf.p10,1/sf.p01];
+vSF = [0,0]; % for debugging
+subplot(2,2,4)
+c = compass([v(1),vSF(1),vIMU(1)],[v(2),vSF(2),vIMU(2)]);
+set(c, {'Color'}, num2cell(lines(3),2))
+legend('est', 'sf', 'gt')
+title('Flow')
