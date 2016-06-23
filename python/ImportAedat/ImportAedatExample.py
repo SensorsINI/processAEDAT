64c18e8caaf5acf780f5524fba0c2be7a2773582
Sim Bamford <sim.bamford@inilabs.com>
2016-06-23 13:15:25 +0000

    Minor fix
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@8877 b7f4320f-462c-0410-a916-d9f35bb82d52

--- python/ImportAedat/ImportAedatExample.py
+++ python/ImportAedat/ImportAedatExample.py
@@ -7,15 +7,16 @@ Created on Mon May 23 11:38:50 2016
 Example script for how to invoke the ImportAedat function
 """
 
+import sys
+sys.path.append('C:\Users\Sim\Ini\JAER_SVN\scripts\python\ImportAedat')
+
 import ImportAedat
 
 # Create a dict with which to pass in the input parameters.
 args = {}
 
 # Put the filename, including full path, in the 'file' field.
-args['filePathAndName'] = '/home/sim/Recordings/Laser_Scanning_Prj/Dungeon/ActiveRail/2016_05_20_laser_on_calibration_targets_and_rails/laser_rail-2016-05-18_13:50:17.aedat'
-
-args['filePathAndName'] = '/home/sim/caer_out-2016-05-27_18:09:09.aedat'
+args['filePathAndName'] = 'C:\\Users\\Sim\\example.aedat'
 
 # Only read special events
 args['dataTypes'] = {0}
