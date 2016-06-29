c01f2f48e1f01b0d2bd9393094ec988b108dcbdc
Diederik Moeys <diederikmoeys@ini.uzh.ch>
2016-06-29 08:15:23 +0000

    added try catch
    git-svn-id: https://svn.code.sf.net/p/jaer/code/scripts@8912 b7f4320f-462c-0410-a916-d9f35bb82d52

--- python/cAER_utils/imagers_characterization/analysis/APS_qe_slope.py
+++ python/cAER_utils/imagers_characterization/analysis/APS_qe_slope.py
@@ -9,18 +9,12 @@
 # ############################################################
 from __future__ import division
 import os
-import struct
-import threading
 import sys
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-import string
 from pylab import *
-import scipy.stats as st
-import math
 sys.path.append('utils/')
 import load_files
 #import winsound
@@ -35,25 +29,11 @@ class APS_qe_slope:
         if(not os.path.exists(figure_dir)):
             os.makedirs(figure_dir)
         #pixel_area = (18e-6*18e-6)
-        exposure_time_scale = 10e-6
         directory = ptc_dir
         files_in_dir = os.listdir(directory)
         files_in_dir.sort()
         u_y_tot = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])+1*-1
-        sigma_tot = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])+1*-1
         exposures = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])
-        FPN_all = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])+1*-1
-        mean_y_FPN_all = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])+1*-1
-        mean_x_FPN_all = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])+1*-1
-        FPN_in_x_all = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])+1*-1
-        FPN_in_y_all = np.zeros([len(files_in_dir),len(frame_y_divisions),len(frame_x_divisions)])+1*-1
-        FPN_50 = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
-        mean_y_FPN_50 = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
-        mean_x_FPN_50 = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
-        FPN_in_x_50 = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
-        FPN_in_y_50 = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
-        u_y_tot_50perc = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
-        Gain_uVe_log = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
         Gain_uVe_lin = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
         i_pd_es = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
         i_pd_vs = np.zeros([len(frame_y_divisions),len(frame_x_divisions)])
@@ -87,38 +67,10 @@ class APS_qe_slope:
                     all_frames.append(frame_areas)
                     frame_areas = np.right_shift(frame_areas,6)
                     n_frames, ydim, xdim = np.shape(frame_areas)   
-                    #avr_all_frames = []
-                    #for this_frame in range(n_frames):
-                    #    avr_all_frames.append(np.mean(frame_areas[this_frame]))
-                    #avr_all_frames = np.array(avr_all_frames)       
-                    #u_y = (1.0/(n_frames*ydim*xdim)) * np.sum(np.sum(frame_areas,0))  # 
                     ydim_f , xdim_f = np.shape(frame_areas[0])
                     temporal_mean = np.zeros([ydim_f, xdim_f])
-                    temporal_variance = np.zeros([ydim_f, xdim_f])
-                    #temporal_variance_EMVA = 0
-                    #factor = (1.0/(2.0*float(xdim_f*ydim_f)))
-                    #raise Exception
-                    for tx in range(xdim_f):
-                        for ty in range(ydim_f):
-                            temporal_mean[ty,tx] = np.mean(frame_areas[:,ty,tx])
-                            temporal_variance[ty,tx] =  np.sum((frame_areas[:,ty,tx]-temporal_mean[ty,tx])**2)/n_frames
-                            #temporal_variance_EMVA = temporal_variance_EMVA + (frame_areas[10,ty,tx]- frame_areas[11,ty,tx])**2.0
-                    #temporal_variance_EMVA = factor*temporal_variance_EMVA
-                    sigma_y = np.mean(temporal_variance)
-                    #sigma_y = temporal_variance_EMVA
                     spatio_temporal_mean = np.mean(np.mean(temporal_mean,0),0)
-                    spatio_var_temporal_mean = 0.0
-                    y_var_temporal_mean = np.zeros([xdim_f])
-                    x_var_temporal_mean = np.zeros([ydim_f])
-                    y_temporal_mean = np.mean(temporal_mean,0)
-                    x_temporal_mean = np.mean(temporal_mean,1)
-                    spatio_var_y_temporal_mean = 0.0
-                    spatio_var_x_temporal_mean = 0.0
-                    
-
-#                    print(str(np.shape(all_frames)) + " all_frames.")
                     u_y_tot[this_file, this_div_y, this_div_x] = spatio_temporal_mean
-                    sigma_tot[this_file, this_div_y, this_div_x] = sigma_y
                     exposures[this_file, this_div_y, this_div_x] = exp
                             
         if(ptc_dir.lower().find('debug') < 0):        
@@ -129,21 +81,7 @@ class APS_qe_slope:
             exposures = np.reshape(exposures_real, [files_num-to_remove, y_div, x_div])
             u_y_tot_real = u_y_tot[u_y_tot != -1]
             u_y_tot =  np.reshape(u_y_tot_real, [files_num-to_remove, y_div, x_div])
-
             exposures = exposures[:,0,0]
-            #all_frames = np.array(all_frames)
-            #plt.figure()
-            #plt.title("all frames values")
-            #for i in range(len(all_frames)):
-            #    this_ff = np.reshape(all_frames[i], len(all_frames[i]))
-            #    this_dn_f = np.right_shift(this_ff,6)
-            #    plot(this_dn_f) 
-            #plt.xlabel("frame number")   
-            #plt.legend(loc='best')
-            #plt.xlabel('frame number') 
-            #plt.ylabel('DN value single pixel') 
-            #plt.savefig(figure_dir+"dn_value_single_pixel.pdf",  format='pdf') 
-            #plt.savefig(figure_dir+"dn_value_single_pixel.png",  format='png')  
         
             # Sensitivity plot 
             plt.figure()
@@ -179,13 +117,17 @@ class APS_qe_slope:
                     u_y_fit = u_y_tot[indmin20perc:indmax80perc,this_area_y, this_area_x]
                     exposures_t = np.array(exposures.reshape(len(exposures)))
                     exposures_fit = exposures_t[indmin20perc:indmax80perc]
-                    slope, inter = np.polyfit(exposures_fit.reshape(len(exposures_fit)), u_y_fit.reshape(len(u_y_fit)),1)
-                    fit_fn = np.poly1d([slope, inter])
-                    i_pd_es[this_area_y,this_area_x] = slope*1000000.0*(ADC_range/ADC_values)/(Gain_uVe_lin[this_area_y,this_area_x]/1000000.0)
-                    i_pd_vs[this_area_y,this_area_x] = slope*1000000.0*(ADC_range/ADC_values)
-                    print "Photodiode current is: " + str(slope*1000000.0) + " DN/s or " + str(i_pd_es[this_area_y,this_area_x]) + " e/s or " + str(i_pd_vs[this_area_y,this_area_x]) + " V/s or " + str(i_pd_es[this_area_y,this_area_x]*1.6*10**(-19)) + " A for X: " + str(frame_x_divisions[this_area_x]) + ', Y: ' + str(frame_y_divisions[this_area_y])
-                    ax.plot(exposures_t, u_y_tot[:,this_area_y, this_area_x], 'o--', color=colors[color_tmp], label='X: ' + str(frame_x_divisions[this_area_x]) + ', Y: ' + str(frame_y_divisions[this_area_y]) +' photodiode current: '+ str(format(i_pd_es[this_area_y,this_area_x], '.2f')) + ' e/s or ' + str(format(i_pd_vs[this_area_y,this_area_x], '.2f')) + ' V/s ')
-                    ax.plot(exposures_t, fit_fn(exposures_t), '-*', markersize=4, color=colors[color_tmp])
+                    try: 
+                        slope, inter = np.polyfit(exposures_fit.reshape(len(exposures_fit)), u_y_fit.reshape(len(u_y_fit)),1)
+                        fit_fn = np.poly1d([slope, inter])
+                        i_pd_es[this_area_y,this_area_x] = slope*1000000.0*(ADC_range/ADC_values)/(Gain_uVe_lin[this_area_y,this_area_x]/1000000.0)
+                        i_pd_vs[this_area_y,this_area_x] = slope*1000000.0*(ADC_range/ADC_values)
+                        print "Photodiode current is: " + str(slope*1000000.0) + " DN/s or " + str(i_pd_es[this_area_y,this_area_x]) + " e/s or " + str(i_pd_vs[this_area_y,this_area_x]) + " V/s or " + str(i_pd_es[this_area_y,this_area_x]*1.6*10**(-19)) + " A for X: " + str(frame_x_divisions[this_area_x]) + ', Y: ' + str(frame_y_divisions[this_area_y])
+                        ax.plot(exposures_t, u_y_tot[:,this_area_y, this_area_x], 'o--', color=colors[color_tmp], label='X: ' + str(frame_x_divisions[this_area_x]) + ', Y: ' + str(frame_y_divisions[this_area_y]) +' photodiode current: '+ str(format(i_pd_es[this_area_y,this_area_x], '.2f')) + ' e/s or ' + str(format(i_pd_vs[this_area_y,this_area_x], '.2f')) + ' V/s ')
+                        ax.plot(exposures_t, fit_fn(exposures_t), '-*', markersize=4, color=colors[color_tmp])
+                    except ValueError:
+                        print("Poly Fit Failed for this recording.. skipping")
+                        continue
                     bbox_props = dict(boxstyle="round,pad=0.3", fc="white", ec="black", lw=2)
                     color_tmp = color_tmp+1
             color_tmp = 0;
@@ -212,25 +154,8 @@ class APS_qe_slope:
                     out_file.write("Dark current is: " + str(format(i_pd_es[this_area_y,this_area_x], '.4f')) + " e/s or " + str(i_pd_vs[this_area_y,this_area_x])  + " v/s or " + str(i_pd_es[this_area_y,this_area_x]*1.6*10**(-19))+ " A\n")
                 else:
                     out_file.write("Photodiode current is: " + str(format(i_pd_es[this_area_y,this_area_x], '.4f')) + " e/s or " + str(i_pd_vs[this_area_y,this_area_x])  + " v/s or " + str(i_pd_es[this_area_y,this_area_x]*1.6*10**(-19))+ " A\n")
-        
         out_file.close()
         if(ptc_dir.lower().find('dark') < 0):
             return i_pd_vs, Gain_uVe_lin
         else:
             return i_pd_vs
-
-#        winsound.Beep(500,2000)
-
-    def confIntMean(self, a, conf=0.95):
-        mean, sem, m = np.mean(a), st.sem(a), st.t.ppf((1+conf)/2., len(a)-1)
-        return mean - m*sem, mean + m*sem
-
-    def ismember(self, a, b):
-        '''
-        as matlab: ismember
-        '''
-        # tf = np.in1d(a,b) # for newer versions of numpy
-        tf = np.array([i in b for i in a])
-        u = np.unique(a[tf])
-        index = np.array([(np.where(b == i))[0][-1] if t else 0 for i,t in zip(a,tf)])
-        return tf, index
